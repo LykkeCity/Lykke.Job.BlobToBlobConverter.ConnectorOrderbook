0c148b86a97ae843b020be7682d0b21d46bbc38f
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-05-02 13:03:01 +0300

    Nuget updates + auto-registration in monitoring

--- src/Lykke.Job.BlobToBlobConverter.ConnectorOrderbook/Settings/AppSettings.cs
+++ src/Lykke.Job.BlobToBlobConverter.ConnectorOrderbook/Settings/AppSettings.cs
@@ -8,6 +8,8 @@ namespace Lykke.Job.BlobToBlobConverter.ConnectorOrderbook.Settings
         public BlobToBlobConverterConnectorOrderbookSettings BlobToBlobConverterConnectorOrderbookJob { get; set; }
 
         public SlackNotificationsSettings SlackNotifications { get; set; }
+
+        public MonitoringServiceClientSettings MonitoringServiceClient { get; set; }
     }
 
     public class SlackNotificationsSettings
@@ -20,6 +22,12 @@ namespace Lykke.Job.BlobToBlobConverter.ConnectorOrderbook.Settings
         public string ConnectionString { get; set; }
 
         public string QueueName { get; set; }
+    }
+
+    public class MonitoringServiceClientSettings
+    {
+        [HttpCheck("api/isalive")]
+        public string MonitoringServiceUrl { get; set; }
     }
 
     public class BlobToBlobConverterConnectorOrderbookSettings
